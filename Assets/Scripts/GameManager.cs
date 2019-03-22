f406becd26f3e59898795bead4780a2d434a6067
Zaski <zaski123@gmail.com>
2019-03-22 10:50:34 +0100

    Added initial objects and blank scripts for the game

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	#region VARIABLES
+	public TMPro.TextMeshProUGUI counter;	// Contador de galletas
+	#endregion
+	
+	#region START
+	private void Start()
+	{
+		currentNumber = 0;
+		currentMultiplier = 1;
+		currentBase = 1;
+		//currentMultiplierAutocklick = 1;
+		//currentUPS = 0;
+	}
+	#endregion
+	
+	#region UPDATE
+
+	private void Update()
+	{
+		
+	}
+
+	#endregion
+}
